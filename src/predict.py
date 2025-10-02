6ee9fe857a1157b7d3e6d8faa68798bb9ecb8018
Maltine <maltina.rama@gmail.com>
2025-10-02 19:58:18 +0200

    predict category

---
+++ src/predict.py
@@ -0,0 +1,18 @@
+import joblib
+from preprocess import preprocess_text
+
+
+clf = joblib.load('models/RandomForestClassifier.sav')
+vectorizer = joblib.load('models/vectorizer.sav')
+category_to_num = joblib.load('models/category_to_num.sav')
+num_to_category = {v: k for k, v in category_to_num.items()}
+
+def predict_category(text):
+    processed = preprocess_text(text)
+    vectorized = vectorizer.transform([processed])
+    pred_num = clf.predict(vectorized)[0]
+    return num_to_category[pred_num]
+
+if __name__ == "__main__":
+    sample = "Wireless Bluetooth Headphones with mic"
+    print("Predicted category:", predict_category(sample))
